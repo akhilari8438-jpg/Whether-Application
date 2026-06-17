246c1b436056c1be643facc9891aec73cdac6b54
Arati khilari <akhilari8438@gmail.com>
2026-06-18 00:29:07 +0530

    Add files via upload

---
+++ whetherapplication/script.js
@@ -0,0 +1,21 @@
+async function getWeatherData(){
+    const cityNAme =document.querySelector("#cityInput").value
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityNAme}&units=metric&appid=ca018df54353f065aaed7d802825b8be`)
+    .then(res=>res.json())
+    .then(data=>renderData(data))
+    document.querySelector("#cityInput").value = ''
+}
+
+function renderData(data){
+    document.querySelector("#dataResult").innerHTML = `
+                    <h3>${data.name} (<i>${data.sys.country}</i>)</h3>
+                    <p><b>Temprature</b> : ${data.main.temp} <sup>0</sup> C</p>
+                    <p>
+                    <b class="">${data.weather[0].main} </b> : ${data.weather[0].description}
+                    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
+                    </p>
+                    <p><b>Wind Speed</b>:${data.wind.speed} km/h</p>
+                    <p><b>Pressure</b>:${data.main.pressure} hPa</p>
+                    <p><b>Humidity</b>:${data.main.humidity} %</p>
+    `
+}
